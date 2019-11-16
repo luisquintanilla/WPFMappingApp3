21c3cd906411b3aede81f10a34fe4789cabff33c
Luis Quintanilla <luquinta@microsoft.com>
2019-11-16 00:51:57 -0500

    Initial commit

---
+++ WPFMappingApp3/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.FileExtensions;
+using Microsoft.Extensions.Configuration.Json;
+using System.CodeDom;
+
+namespace WPFMappingApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly IConfiguration Config;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+        }
+
+        private async void MapControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            BasicGeoposition cityPosition = new BasicGeoposition() { Latitude = 47.604, Longitude = -122.329 };
+            Geopoint cityCenter = new Geopoint(cityPosition);
+            MyMap.Style = MapStyle.Aerial;
+            MyMap.MapServiceToken = Config["ServiceToken"];
+            await MyMap.TrySetViewAsync(cityCenter);
+        }
+    }
+}
